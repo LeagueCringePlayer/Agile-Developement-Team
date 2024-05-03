ef10178d6140f7debe43fcd1cb611a6bdf125f17
LeagueCringePlayer <mouadhytler@gmail.com>
2024-05-03 13:59:33 +0100

    I added a test for the order class where it checks if an order exists or do not exist

---
+++ Testing2/tstOrder.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create the instance 
+            clsOrder AnOrder = new clsOrder();
+            // test the existence 
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
