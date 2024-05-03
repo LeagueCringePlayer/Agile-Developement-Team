1decd52cbb67466bc896089683435ba574d5737b
zainS09 <zainsacranie@hotmail.co.uk>
2024-05-03 14:06:42 +0100

    Added testing for stock class

--- AdminSystem/StockConfirmDelete.aspx
+++ AdminSystem/StockConfirmDelete.aspx
@@ -7,6 +7,7 @@
     <title></title>
 </head>
 <body>
+    This is the Stock Confirm Delete Page
     <form id="form1" runat="server">
         <div>
         </div>
---
+++ Testing5/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the new class to create
+            clsStock StockItem = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(StockItem);
+        }
+    }
+}
