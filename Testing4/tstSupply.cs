6f9fe41c152f04538255e89a7381b6df16a4cb92
P2709563 <P2709563@my365.dmu.ac.uk>
2024-05-07 03:27:35 +0100

    The changes has been done by Sarvarbek. Created my first test method, successfully merged my branch to the main repository. Created my own branch, pushed the branch, created a pull request and then finally deleted the branch.

---
+++ Testing4/tstSupply.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstSupply
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsSupply ASupply = new clsSupply();
+            //test to see that it exists
+            Assert.IsNotNull(ASupply);
+        }
+    }
+}
