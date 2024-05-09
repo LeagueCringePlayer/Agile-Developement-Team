329a4bce900cd570ec15fa135b8d0332d6f5af92
LeagueCringePlayer <mouadhytler@gmail.com>
2024-05-09 02:09:42 +0100

    Hello this are the tests for my properties they work fine .

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Order_Arrival { get; set; }
+        public int StockID { get; set; }
+        public int StaffID { get; set; }
+        public int CustomerID { get; set; }
+        public string ShippingAdress { get; set; }
+        public string PaymentMethod { get; set; }
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
     }
 }
