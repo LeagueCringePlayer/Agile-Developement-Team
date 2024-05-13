615877d2f588aca6eb39c081f7fc760884aebb79
youcefabd <youcefabdennebi99@gmail.com>
2024-05-13 09:09:30 +0100

    this is the test that i had to create from leb 7

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public clsStaff()
+        {
+        }
+
+        public string StaffName { get; set; }
+        public string StaffEmail { get; set; }
+        public string StaffRole { get; set; }
+        public DateTime DateHired { get; set; }
+        public bool IsActive { get; set; }
+        public decimal HourlyRate { get; set; }
+    }
+}
