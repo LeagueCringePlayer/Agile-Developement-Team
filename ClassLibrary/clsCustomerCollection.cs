268c811bcdca903f2f580a24961dd7010462c6a8
p2712079 <p2712079@my365.dmu.ac.uk>
2024-05-15 14:00:24 +0100

    Collection Class for Customer Working

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return CustomerList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsCustomer ThisCustomer { get; set; }
+        
+
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+                ACustomer.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                ACustomer.FullName = Convert.ToString(DB.DataTable.Rows[Index]["FullName"]);
+                ACustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                ACustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                // Check for DBNull before converting to DateTime
+                if (DB.DataTable.Rows[Index]["DateOfBirth"] != DBNull.Value)
+                {
+                    ACustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfBirth"]);
+                }
+                else
+                {
+                    // Handle DBNull case appropriately, maybe assign a default value or handle it differently
+                    ACustomer.DateOfBirth = DateTime.MinValue; // For example, assign a default value
+                }
+                ACustomer.Password = Convert.ToString(DB.DataTable.Rows[Index]["Password"]);
+                ACustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]); 
+                mCustomerList.Add(ACustomer);
+                Index++;
+
+            }
+        }
+
+
+    }
+}
---
+++ Testing1/tstCustomerCollection.cs
@@ -0,0 +1,77 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);  
+        }
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.CustomerId = 1;
+            TestItem.FullName = "Jainik Ratilal";
+            TestItem.Address = "DMU Road";
+            TestItem.Email = "Jainikratilal@gmail.com";
+            TestItem.DateOfBirth = Convert.ToDateTime("17/03/2004");
+            TestItem.Password = "Password123";
+            TestItem.Active = true;
+            TestList.Add(TestItem); 
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+        }
+       
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            clsCustomer TestCustomer = new clsCustomer();
+            
+            TestCustomer.CustomerId = 1;
+            TestCustomer.FullName = "Jainik Ratilal";
+            TestCustomer.Address = "DMU Road";
+            TestCustomer.Email = "Jainikratilal@gmail.com";
+            TestCustomer.DateOfBirth = Convert.ToDateTime("17/03/2004");
+            TestCustomer.Password = "Password123";
+            TestCustomer.Active = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            
+            TestItem.CustomerId = 1;
+            TestItem.FullName = "Jainik Ratilal";
+            TestItem.Address = "DMU Road";
+            TestItem.DateOfBirth = Convert.ToDateTime("17/03/2004");
+            TestItem.Email = "Jainikratilal@gmail.com";
+            TestItem.Password = "Password123";
+            TestItem.Active = true;
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+       
+       
+        
+    }
+}
