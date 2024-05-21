64605d2800333dccf9ad31e63f2fa478a815180a
zainS09 <zainsacranie@hotmail.co.uk>
2024-05-21 10:53:13 +0100

    added collection class

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+
+        //private data member for the list
+        List<clsStock> mStockList = new List<clsStock>();   
+
+
+        public List<clsStock> StockList 
+        {
+            get
+            {
+                //return private data member
+                return mStockList;
+            }
+            set
+            {
+                //set the private data
+                mStockList = value; 
+            } 
+        }
+
+        public int Count 
+        {
+            get
+            {
+                //return the count of the list
+                return mStockList.Count;
+            }
+            set
+            {
+                //to be done later
+            } 
+        }
+        public clsStock ThisStock { get; set; }
+    }
+}
