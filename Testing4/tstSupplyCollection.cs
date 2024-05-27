b763c19fc9b4910b21a01506113b1760089c4eb9
P2709563 <P2709563@my365.dmu.ac.uk>
2024-05-27 11:21:57 +0100

    Creating the Collection Class Methods - Delete & Filter

--- Testing4/tstSupplyCollection.cs
+++ Testing4/tstSupplyCollection.cs
@@ -152,7 +152,93 @@ namespace Testing4
             AllSupplies.ThisSupply.Find(PrimaryKey);
             //test to see if ThisSupply matches the test data
             Assert.AreEqual(AllSupplies.ThisSupply, TestItem);
+        }
 
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplyCollection AllSupplies = new clsSupplyCollection();
+            //create the item of test data
+            clsSupply TestItem = new clsSupply();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.SupplyID = 2;
+            TestItem.SupplierContact = "p1234567@my365.dmu.ac.uk";
+            TestItem.PriceOfResource = 1009;
+            TestItem.DateRequested = DateTime.Now;
+            TestItem.AvailabilityOfSupplier = true;
+            TestItem.ToBeDeliveredBy = DateTime.Now;
+            //set ThisSupply to the test data
+            AllSupplies.ThisSupply = TestItem;
+            //add the record
+            PrimaryKey = AllSupplies.Add();
+            //set the primary key of the test data
+            TestItem.SupplyID = PrimaryKey;
+            //find the record
+            AllSupplies.ThisSupply.Find(PrimaryKey);
+            //delete the record
+            AllSupplies.Delete();
+            //now find the record
+            Boolean Found = AllSupplies.ThisSupply.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void ReportBySupplierContactOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsSupplyCollection AllSupplies = new clsSupplyCollection();
+            //create an instance of the filtered data
+            clsSupplyCollection FilteredSupplies = new clsSupplyCollection();
+            //apply a blank string (should return all records);
+            FilteredSupplies.ReportBySupplierContact("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllSupplies.Count, FilteredSupplies.Count);
+        }
+
+        [TestMethod]
+        public void ReportBySupplierContactNoneFound()
+        {
+            //create an instance of the class we want to create
+            clsSupplyCollection FilteredSupplies = new clsSupplyCollection();
+            //apply a Supplier Contact that doesn't exist
+            FilteredSupplies.ReportBySupplierContact("abc def");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredSupplies.Count);
+        }
+
+        [TestMethod]
+        public void ReportBySupplierContactTestDataFound()
+        {
+            //create an instance of the filtered data
+            clsSupplyCollection FilteredSupplies = new clsSupplyCollection();
+            //variable to store the outcome
+            Boolean OK = true;
+            //apply a Supplier Contact that doesn't exist
+            FilteredSupplies.ReportBySupplierContact("p1122556@my365.dmu.ac.uk");
+            //check that the correct number of records are found
+            if (FilteredSupplies.Count == 2)
+            {
+                //check to see that the first record is 52
+                if (FilteredSupplies.SupplyList[0].SupplyID != 52)
+                {
+                    OK = false;
+                }
+                //check to seee that the first record is 53
+                if (FilteredSupplies.SupplyList[1].SupplyID != 53)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            //test to see that there are no records
+            Assert.IsTrue(OK);
         }
     }
 }
